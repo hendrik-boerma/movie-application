18c22d3ae366172f1104e9ed6af963ed038fbc08
Hendrik Boerma <hendrikboerma@gmail.com>
2024-05-16 17:36:40 +0200

    test home

--- src/Components/Home.test.js
+++ src/Components/Home.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Home from './Home';
+import App from '../App';
 
 describe('Highlight movies', () => {
     it('Renders the movies "Toy Story" and "Forrest Gump" on page load', async () => {
-      const { findByText } = render(<Home />);
+      const { findByText } = render(<App />);
   
       await findByText('Uitgelicht');
   
